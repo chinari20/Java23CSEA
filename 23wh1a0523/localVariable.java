63e446bc2a867b40f3889eee186a10c98b2fc826
23wh1a0523 <150155882+23wh1a0523@users.noreply.github.com>
2024-08-09 16:16:44 +0530

    Create localVariable.java

---
+++ 23wh1a0523/localVariable.java
@@ -0,0 +1,27 @@
+package javapractice1;
+
+
+
+public class variableLocal {
+
+	public static void main(String args[]) { 
+
+			int num = 23;    //local variables should be intialized
+
+			//for local variables memory is created in stack
+
+			//inside the method
+
+			//this variable is valid in this method only
+
+			System.out.println(num); 
+
+			
+
+		
+
+	}
+
+
+
+}
