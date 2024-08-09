8295a611b17bfd0093f7d0ee4de5eaf6659f84ea
23wh1a0518 <150670102+23wh1a0518@users.noreply.github.com>
2024-08-09 16:04:51 +0530

    Create LocalVariable.java

---
+++ 23WH1A0518/LocalVariable.java
@@ -0,0 +1,15 @@
+package practicepack;
+
+public class LocalVariable {
+	public static void main(String[] args) {
+		String StudentName = "Diya";
+		int num1 = 14;
+		System.out.println(StudentName);
+		System.out.println(num1);
+
+
+		// TODO Auto-generated method stub
+
+	}
+	
+}
