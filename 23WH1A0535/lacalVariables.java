0263c35984f37b9f349b662fe6ad434fef96f05a
Sadhana Kalaga <150142098+23wh1a0535-sadhana@users.noreply.github.com>
2024-08-09 15:44:02 +0530

    Create lacalVariables.java

---
+++ 23WH1A0535/lacalVariables.java
@@ -0,0 +1,9 @@
+package Practice;
+
+public class localVar {
+	public static void main(String[] args) {
+		int num;
+		System.out.println(num);/*local variable cannot be accessed without initialization ,memory is allocated in the stack for local variables*/
+
+}
+}
