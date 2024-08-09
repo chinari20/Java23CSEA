2d2cfccb740e2f577fd15be52231ade52dc06070
23wh1a0531 <150212990+23wh1a0531@users.noreply.github.com>
2024-08-09 15:45:56 +0530

    Create LocalVariables.java

---
+++ 23WH1A0531/LocalVariables.java
@@ -0,0 +1,10 @@
+package PracticePackage;
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		int num = 12 ;
+		System.out.println(num);
+	}
+	//System.out.println(num) ;
+	// the3 above line will show error as num is a local variable and can be accessed only in the method it is declared
