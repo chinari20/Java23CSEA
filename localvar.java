726945d9a5cf85d53a47a6eafc49d2ff9f88a3b6
saadia taqveem <150131526+23wh1a0525saadia@users.noreply.github.com>
2024-08-12 09:17:15 +0530

    Add files via upload

---
+++ localvar.java
@@ -0,0 +1,6 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        int num = 5;
+        System.out.println(num);
+    }
+}
---
+++ staticvar.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    static int num = 5;
+    public static void main(String[] args) {
+        HelloWorld obj1 = new HelloWorld();
+        obj1.num = 7;
+        System.out.println(obj1.num);
+        HelloWorld obj2 = new HelloWorld();
+        System.out.println(obj2.num);
+    }
+}
