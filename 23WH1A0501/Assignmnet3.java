bdf6228ac1dd84d54833e1abbcf006f6b38f027c
Gnanika Omkarini Makkena <150130605+23WH1A0501@users.noreply.github.com>
2024-08-21 14:47:12 +0530

    Create Assignmnet3.java

---
+++ 23WH1A0501/Assignmnet3.java
@@ -0,0 +1,17 @@
+package CSEA501;
+import java.util.*;
+public class Assignment3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the number of numbers being entered: ");
+		int length = sc.nextInt();
+		int oddSum = 0 ; 
+		int evenSum = 0;
+		for(int iteration = 1 ; iteration <= length ; iteration++) {
+			int num = sc.nextInt();
+			if(num % 2  == 0) evenSum += num;
+			else oddSum += num;
+		}
+		System.out.printf("Even sum =  %d\nOdd Sum = %d" , evenSum , oddSum);
+	}
+}
