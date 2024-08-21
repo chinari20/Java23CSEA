cfb26031571c3a40d5a7ce635422e14f988ff4fa
23wh1a0561 <147315586+23wh1a0561@users.noreply.github.com>
2024-08-21 14:41:20 +0530

    Add files via upload

---
+++ 23WH1A0561/ArrayDemo.java
@@ -0,0 +1,13 @@
+package program61;
+
+public class ArrayDemo {
+	public static void main(String[] args){
+		 int studentList[] = {10,55,23,94,25};
+		 System.out.println(studentList[0]);
+		 System.out.println("size of array:" + studentList.length);
+		 for(int i = 0; i< studentList.length;i++) {
+		 	System.out.print(studentList[i] + "\t");
+		 		}
+
+		 	}
+	}
