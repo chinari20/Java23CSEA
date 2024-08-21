f3ac9742af97c2342fce41a41ecde26f41bd3c18
23WH1A0502 <150150254+23WH1A0502@users.noreply.github.com>
2024-08-21 13:52:49 +0530

    Create Array.java

---
+++ 23WH1A0502/Array.java
@@ -0,0 +1,16 @@
+
+package program;
+
+public class array {
+
+	public static void main(String[] args) {
+		int studentlist[] = new int[5];
+		studentlist[0] = 501;
+		studentlist[1] = 502;
+		studentlist[2] = 503;
+		System.out.println(studentlist[0]);
+		System.out.println(studentlist[3]);
+
+	}
+
+}
