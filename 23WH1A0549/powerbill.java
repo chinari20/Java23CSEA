58e93ad0480bcbf8306e574bf15f00c492b24c62
23WH1A0549 <150163872+23WH1A0549@users.noreply.github.com>
2024-08-21 14:41:28 +0530

    Add files via upload

---
+++ 23WH1A0549/powerbill.java
@@ -0,0 +1,42 @@
+package student;
+import java.util.Scanner;
+
+public class powerbill {
+
+	public static void main(String[] args) {
+		int temp,charge;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the old reading");
+		int oldReading=sc.nextInt();
+		System.out.println("enter the new reading");
+		int newReading=sc.nextInt();
+		int units=newReading-oldReading;
+		if(units<=50) {
+			System.out.println("charge is"+units*1);
+		}
+		else if(units>50 && units<=100) {
+			temp=units-50;
+		    charge=temp*2+50;
+			System.out.println("charge "+charge);
+		}
+		else if(units > 100 && units<=200) {
+			temp=units-100;
+			charge=temp*3+150;
+			System.out.println("charge "+charge);
+			
+		}
+		else if(units > 200 && units <=400) {
+			temp=units-200;
+			charge=temp*4+450;
+			System.out.println("charge "+charge);
+		}
+		else {
+			temp=units-400;
+			charge=temp*5+1250;
+			System.out.println("charge "+charge);
+		}
+		
+
+	}
+
+}
