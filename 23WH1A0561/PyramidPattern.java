b823a724a6cf2878f97dc1dffb80411e0e975002
23wh1a0561 <147315586+23wh1a0561@users.noreply.github.com>
2024-08-21 14:55:28 +0530

    Add files via upload

---
+++ 23WH1A0561/PyramidPattern.java
@@ -0,0 +1,29 @@
+package program61;
+
+public class PyramidPattern {
+		public static void main(String args[]) {
+			int i,j,k;
+			for(i = 1; i <= 7 ;i++) {
+				for(j = 1; j <= 7 - i; j++) {
+					System.out.print(" ");
+				}
+			
+				for(k = 1; k <= i*2 - 1; k++) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+			for(i = 6; i >= 1 ; i--) {
+				for(j = 7 - i; j >= 1; j--) {
+					System.out.print(" ");
+				}
+			
+				for(k = i*2 - 1; k >= 1; k--) {
+					System.out.print("*");
+				}
+				System.out.println();
+			}
+
+		}
+	}
+
