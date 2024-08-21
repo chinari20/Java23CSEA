f3dd3bc591fc9240be005596e7b30d2563581359
23wh1a0554 <150828294+23wh1a0554@users.noreply.github.com>
2024-08-21 14:39:59 +0530

    Add files via upload

---
+++ StaticProgram.java
@@ -0,0 +1,41 @@
+package cse554;
+
+public class StaticProgram {
+
+			static String classRoomNumber="DB304";
+
+			static int num1;
+
+			public static void main(String[] args) {
+
+				int num;
+
+				StaticProgram obj1=new StaticProgram();
+
+				StaticProgram obj2=new StaticProgram();
+
+				obj1.classRoomNumber="newstone305";
+
+				System.out.println(StaticProgram.classRoomNumber);
+
+				System.out.println(obj2.classRoomNumber);
+
+				System.out.println(StaticProgram.num1);
+
+
+
+			}
+
+			public String getAttendance(String rollnum) {
+
+				System.out.println(classRoomNumber);
+
+
+
+				return rollnum;
+
+			}
+
+
+	}
+
