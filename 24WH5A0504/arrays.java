0788384decdfdb9a1834f338b888ed0b39a0dbe6
allaangel <24wh5a0504@bvrithyderabad.edu.in>
2024-08-21 14:46:53 +0530

    Add files via upload

---
+++ 24WH5A0504/arrays.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class arrays {
+
+	public static void main(String[]args)
+
+	{
+
+		
+		int studentlist[]=new int [5];
+studentlist[0]=501;
+studentlist[1]=503;
+studentlist[2]=504;
+System.out.println(studentlist[0]);
+System.out.println(studentlist[1]);
+System.out.println(studentlist[2]);
+}
+
+}
