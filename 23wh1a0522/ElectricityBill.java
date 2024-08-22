bf8b37c7aae264453fe1b9b9daf164861cf36138
SIRISHA <150130671+23WH1A0522@users.noreply.github.com>
2024-08-22 22:51:59 +0530

    Create ElectricityBill.java
     JavaSE application to generate Electricity bill   (use if-else-if ladder)
    inputs are old reading and current reading and generate the bill by following  slabs/ranges: Scan the inputs using Scanner class
    0-50: Rs.1
    50-100: Rs2
    100-200: Rs.3
    200-400: Rs.4
    >400: Rs.5
    example if the number of units are 120 then the consumption charge is (50*1)+(50*2)+(20*3)=Rs.210

---
+++ 23wh1a0522/ElectricityBill.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class ElectricityBill {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the old reading of electricity bill: ");
+        int oldReadings = scanner.nextInt();
+        System.out.print("Enter the new reading of electricity bill: ");
+        int newReadings = scanner.nextInt();
+        int units = newReadings - oldReadings;
+        int temp_units = 0;
+        int cost = 0;
+        if(units>400){
+            temp_units = units - 400;
+            cost =  (temp_units * 5) + 1250;
+        }
+        else if(units>200 && units<=400){
+            temp_units = units - 200;
+            cost = (temp_units * 4) + 450;
+        }
+        else if(units>100 && units<=200){
+            temp_units = units - 100;
+            cost = (temp_units * 3) + 150;
+        }
+        else if(units>50 && units<=100){
+            temp_units = units - 50;
+            cost = (temp_units * 2) + 50;
+        }
+        else{
+            cost = units;
+        }
+        System.out.printf("Number of units consumed : %d\n",units);
+        System.out.printf("Electricity bill : %d/-",cost);
+    }
+}
