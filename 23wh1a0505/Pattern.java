af8f612a58c9570f151c2b5792b6f9112cbaff5f
23WH1A0505 <150135423+23WH1A0505@users.noreply.github.com>
2024-08-23 12:00:41 +0530

    Add files via upload

---
+++ 23wh1a0505/Pattern.java
@@ -0,0 +1,25 @@
+package programme;
+import java.util.*;
+
+
+	import java.util.Scanner;
+
+	public class Pattern {
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter the number of lines: ");
+	        int n = scanner.nextInt();
+
+	        for (int i = 1; i <= n; i++) {
+	            for (int j = i; j >= 1; j--) {
+	                System.out.print(j);
+	            }
+	            for (int j = 2; j <= i; j++) {
+	                System.out.print(j);
+	            }
+	            System.out.println();
+	        }
+	    }
+	}
+
