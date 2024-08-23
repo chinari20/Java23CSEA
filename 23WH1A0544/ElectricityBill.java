23438f93b8f42d4529da74345fd143e9fd365832
23wh1a0544 <150228254+23wh1a0544@users.noreply.github.com>
2024-08-23 11:50:15 +0530

    Add files via upload

---
+++ 23WH1A0544/ElectricityBill.java
@@ -0,0 +1,34 @@
+package javaprogramsi8;
+
+import java.util.Scanner;
+public class ElectricityBill {
+	public static void main(String[] args) {
+			      	Scanner sc = new Scanner(System.in);
+			        System.out.print("Enter old reading: ");
+			        int oldReading = sc.nextInt();
+			        System.out.print("Enter current reading: ");
+			        int currentReading = sc.nextInt();
+			        int units = currentReading - oldReading;
+			        int BillAmount = 0;
+			        if (units <= 50) {
+			            BillAmount = units * 1;
+			        }
+			        else if (units <= 100) {
+			            BillAmount = (50 * 1) + ((units - 50) * 2);
+			        }
+			        else if (units <= 200) {
+			            BillAmount = (50 * 1) + (50 * 2) + ((units - 100) * 3);
+			        } 
+			        else if (units <= 400) {
+			            BillAmount = (50 * 1) + (50 * 2) + (100 * 3) + ((units - 200) * 4);
+			        } 
+			        else {
+			            BillAmount = (50 * 1) + (50 * 2) + (100 * 3) + (200 * 4) + ((units - 400) * 5);
+			        }
+			  
+			        System.out.println("Total bill amount: Rs." + BillAmount);
+			      
+			        sc.close();
+			 }
+
+	}
