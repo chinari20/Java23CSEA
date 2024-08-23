3b3f453006214f31ba6e74dd030b22eb7bbb0baa
23wh1a0544 <150228254+23wh1a0544@users.noreply.github.com>
2024-08-23 11:55:09 +0530

    Add files via upload

---
+++ 23WH1A0544/SumEvenOdd.java
@@ -0,0 +1,27 @@
+package javaprogramsi8;
+
+import java.util.Scanner;
+public class SumEvenOdd {
+
+	public static void main(String[] args) {
+		int i;
+		int EvenSum = 0;
+		int OddSum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter no of integers ");
+		int limit  = sc.nextInt();
+		for(i = 1;i<=limit;i++) {
+			int n  = sc.nextInt();
+			if (n%2==0) {
+				EvenSum=EvenSum+n;
+			}
+			else {
+				OddSum=OddSum+n;
+			}
+		}
+		System.out.println("Even sum is "+EvenSum);
+		System.out.println("Odd sum is " +OddSum);
+
+
+	}
+}
