a1f98ca67eb8b346d2ac28919a6bcc5405374c29
Nikhitha003 <24wh5a0503@bvrithyderabad.edu.in>
2024-08-23 12:36:09 +0530

    Update sum of even numbers.java

--- 24wh5a0503/sum of even numbers.java
+++ 24wh5a0503/sum of even numbers.java
@@ -1,1 +1,25 @@
+package LE503;
+import java.util.Scanner;
+public class sumOfEvenNumbers{
+    public static void main(String[] args) {
+ Scanner scanner = new Scanner(System.in);
+        int evenSum = 0;
+        int oddSum = 0;
 
+        System.out.println("Enter the number of integers:");
+        int n = scanner.nextInt();
+
+        System.out.println("Enter the integers:");
+        for (int i = 0; i < n; i++) {
+            int num = scanner.nextInt();
+            if (num % 2 == 0) {
+                evenSum += num;
+            } else {
+                oddSum += num;
+            }
+        }
+
+        System.out.println("Sum of even integers: " + evenSum);
+        System.out.println("Sum of odd integers: " + oddSum);
+    }
+}
