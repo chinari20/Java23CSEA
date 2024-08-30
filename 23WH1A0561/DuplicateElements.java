406379059f0bfff2bb9cc11fb8c3084606779db5
23wh1a0561 <147315586+23wh1a0561@users.noreply.github.com>
2024-08-30 11:28:37 +0530

    Add files via upload

---
+++ 23WH1A0561/DuplicateElements.java
@@ -0,0 +1,25 @@
+
+package source;
+import java.util.Scanner;
+public class DuplicateElements {
+		public static void main(String[] args) {
+			int i,j;
+			System.out.println("enter size: ");
+			Scanner sc = new Scanner(System.in);
+			int len = sc.nextInt();
+			System.out.println("enter elements: ");
+			int[] arr = new int[10];
+			for(i = 0;i<len;i++) {
+				arr[i]=sc.nextInt();
+				
+			}
+			for(i = 0;i<len;i++) {
+				for(j = i+1;j<len;j++) {
+					if(arr[i]==arr[j]) {
+						System.out.println(arr[j]);
+						
+					}
+				}
+			}
+		}
+	}
