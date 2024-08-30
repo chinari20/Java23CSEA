36b1977d7e4373fa41d293864cb9c3262ae1e409
23wh1a0504 <150162173+23wh1a0504@users.noreply.github.com>
2024-08-30 12:09:26 +0530

    Create fibnocci.java

---
+++ 23WH1A0504/fibnocci.java
@@ -0,0 +1,34 @@
+package javap;
+import java.util.*;
+public class Fib {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter num");
+		int c ,n=sc.nextInt();
+		int a[] = new int[n] ;
+		int next ;
+		int n1 = 0 ,n2 =1;
+		a[0]=0;
+		a[1]= 1;
+		for(int i=2; i<n ;i++) {
+			next = n1 + n2;
+			a[i] = next;
+			n1 = n2 ;
+			n2 = next ;
+		}
+		for (int i=0;i<n;i++) {
+			c = 0;
+			for(int j=1;j<=a[i];j++) {
+				if(a[i]%j == 0) 
+					c += 1;
+			}
+			if(c == 2 && a[i] <=n) {
+				
+				System.out.println(a[i]);
+		}
+	}
+}
+}
+
+
+
