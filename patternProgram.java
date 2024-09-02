c5d435d50a5e86bf73cfd9a92fd50ef754bae53c
Nikhitha003 <24wh5a0503@bvrithyderabad.edu.in>
2024-09-02 21:43:57 +0530

    Create patternProgram.java

---
+++ patternProgram.java
@@ -0,0 +1,16 @@
+package LE503;
+public class printpattern {
+	public static void main(String args[]) {
+		int rows=5;
+		for(int i = 1; i<=rows; i++) {
+			for(int j = i ;j >=1; j--) {
+				System.out.print(j);
+			}
+			for(int j = 2;j<= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
+
+}
