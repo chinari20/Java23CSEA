9fbb5cf06b0854003e4473c9b968cbc910d9ecf3
23WH1A0515 <150152595+23WH1A0515@users.noreply.github.com>
2024-09-20 10:37:32 +0530

    Create Pattern1.java

---
+++ 23WH1A0515/Pattern1.java
@@ -0,0 +1,44 @@
+import java.util.*; 
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the no of rows:");
+
+		int rows = sc.nextInt();
+
+		for(int i = 0; i < rows; i++) {
+
+			int num = i + 1;
+
+			for(int  j = 0 ; j <= (i * 2) ; j++) {
+
+				System.out.print(num + " ");
+
+				if(i <= j) {
+
+					num++;
+
+				}
+
+				else {
+
+					num-- ;
+
+				}
+
+			}
+
+			System.out.println();
+
+		}
+
+		sc.close();
+
+	}
+
+}
+
