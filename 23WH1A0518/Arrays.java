de7a83bc9ce742f9633b3e85533b7c17c1f3d97a
23wh1a0518 <150670102+23wh1a0518@users.noreply.github.com>
2024-09-20 11:55:15 +0530

    Add files via upload

---
+++ 23WH1A0518/Arrays.java
@@ -0,0 +1,16 @@
+package practicejava;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		int studentList[] = new int[5];
+		studentList[0] = 501;
+		studentList[1] = 502;
+		studentList[2] = 503;
+		System.out.println(studentList[0]);
+		System.out.println(studentList[1]);
+		// TODO Auto-generated method stub
+
+	}
+
+}
