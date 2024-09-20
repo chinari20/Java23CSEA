724fb90f477513b1bac8a273ccaf75caac96c8b2
23wh1a0544 <150228254+23wh1a0544@users.noreply.github.com>
2024-09-20 10:38:23 +0530

    Add files via upload

---
+++ 23WH1A0544/CommonElements.java
@@ -0,0 +1,19 @@
+package source;
+import java.util.*;
+public class CommonElements {
+	
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        char arr1[] = new char[]{'l','z','a','d','e'};
+	        char arr2[] = new char[]{'b','k','l','e','f'};
+	        for(int i = 0;i<5;i++){
+	            for(int j = 0;j<5;j++){
+	                if (arr1[i]==arr2[j]){
+	                    System.out.println(arr1[i]);
+	                    }
+	                }
+	            
+	            }
+	        }
+
+}
