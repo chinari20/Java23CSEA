52ba915988c2feb6356069cdd17294c7d1a14a4c
23wh1a0528 <150130964+23wh1a0528@users.noreply.github.com>
2024-09-26 23:54:24 +0530

    Update q5a_duplicates_in_array.java

--- 23WH1A0528/q5a_duplicates_in_array.java
+++ 23WH1A0528/q5a_duplicates_in_array.java
@@ -1,29 +1,50 @@
 import java.util.Scanner;
-public class duplicates_in_array {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		//reading size of array
-		System.out.print("Enter size of the array: ");
-		int arr_size = sc.nextInt();
-		int arr[] = new int[arr_size]; // declaration of array
-		
-		//reading values into array
-		System.out.print("enter elements into array: ");
-		for(int i=0; i<arr_size; i++) {
-			arr[i] = sc.nextInt();
-		}
-		
+public class q5a_DuplicatesInArray {
 
-		int i;
-		for(i=0; i<arr_size; i++) {
-			for(int j=i+1; j<arr_size; j++) {
-				if (arr[i] == arr[j]) {
-					System.out.println("duplicate elements are: " + arr[j]);
-				}
-			}
-		}	
-		sc.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Reading size of the array
+        System.out.print("Enter size of the array: ");
+        int arr_size = sc.nextInt();
+        int arr[] = new int[arr_size]; // Declaration of the array
+        
+        // Reading values into the array
+        System.out.println("Enter elements into the array: ");
+        for (int i = 0; i < arr_size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        // Array to keep track of duplicates
+        boolean[] printed = new boolean[arr_size];
+        
+        // Finding duplicate elements
+        System.out.println("Duplicate elements are: ");
+        boolean hasDuplicates = false;
+        for (int i = 0; i < arr_size; i++) {
+            if (printed[i]) {
+                // Skip if the element is already printed as a duplicate
+                continue;
+            }
+            boolean isDuplicate = false;
+            for (int j = i + 1; j < arr_size; j++) {
+                if (arr[i] == arr[j]) {
+                    isDuplicate = true;
+                    printed[j] = true; // Mark the duplicate element to avoid printing again
+                }
+            }
+            if (isDuplicate) {
+                System.out.println(arr[i]);
+                hasDuplicates = true;
+            }
+        }
+
+        if (!hasDuplicates) {
+            System.out.println("No duplicates found.");
+        }
+        
+        sc.close();
+    }
 }
+
