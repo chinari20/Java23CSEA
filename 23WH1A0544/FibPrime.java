8c36ee3dcbe892726b092677a31e47810225ee99
23wh1a0544 <150228254+23wh1a0544@users.noreply.github.com>
2024-09-30 14:10:44 +0530

    Add files via upload

---
+++ 23WH1A0544/FibPrime.java
@@ -0,0 +1,30 @@
+package source;
+import java.util.*;
+public class FibPrime {
+		public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("enter num");
+			int count,n=sc.nextInt();
+			int a[] = new int[n] ;
+			int next ;
+			int n1 = 0 ,n2 =1;
+			a[0]=0;
+			a[1]=1;
+			for(int i=2; i<n ;i++) {
+				next = n1 + n2;
+				a[i] = next;
+				n1 = n2 ;
+				n2 = next ;
+			}
+			for(int i=0;i<n;i++) {
+				count = 0;
+				for(int j=1;j<=a[i];j++) {
+					if(a[i]%j == 0) 
+						count += 1;
+				}
+				if(count == 2 && a[i]<=n) {
+					System.out.println(a[i]);
+			}
+		}
+	}
+}
