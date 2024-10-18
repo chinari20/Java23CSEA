cadc120eb202676c9fc81f1693178769456c0979
23wh1a0524 <150163786+23wh1a0524@users.noreply.github.com>
2024-10-18 11:49:39 +0530

    Create inavlidPinException.java

---
+++ 23WH1A0524/inavlidPinException.java
@@ -0,0 +1,7 @@
+package java524;
+
+public class invalidPinException extends Exception {
+	void invalidpinException() {
+		System.out.println("you have not enterd valid pin in three chances");
+	}
+}
