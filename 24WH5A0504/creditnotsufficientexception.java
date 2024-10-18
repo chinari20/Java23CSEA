9bde2a5f09b4ba7b6275cb471bb8b1ca6146cb1e
allaangel <24wh5a0504@bvrithyderabad.edu.in>
2024-10-18 10:47:49 +0530

    Add files via upload

---
+++ 24WH5A0504/creditnotsufficientexception.java
@@ -0,0 +1,68 @@
+package javaprograms;
+import java.util.*;
+class CreditsNotSufficientException extends Exception {
+
+public CreditsNotSufficientException(String message) {
+
+     super(message);  
+
+ }
+
+}
+public class creditnotsufficientexception {
+ public static void checkCredits(int credits) throws CreditsNotSufficientException {
+
+     if (credits < 50) {
+
+         throw new CreditsNotSufficientException("Credits are insufficient. You need at least 50 credits.");
+
+     }
+
+ }
+
+ public static void performOperation(int credits) throws CreditsNotSufficientException {
+
+     
+
+     checkCredits(credits);
+
+     
+
+     
+
+     System.out.println("Operation performed successfully with " + credits + " credits.");
+
+ }
+
+
+
+ public static void main(String[] args) {
+
+	 Scanner scanner = new Scanner(System.in);
+
+	 System.out.print("Enter your credits: ");
+
+     int userCredits = scanner.nextInt(); 
+
+   
+
+     try {
+
+         
+
+         performOperation(userCredits);
+
+     } catch (CreditsNotSufficientException e) {
+
+         
+
+         System.out.println(e.getMessage());  
+
+     }
+
+ }
+
+}
+
+
+
