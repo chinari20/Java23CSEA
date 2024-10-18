cb77c39de196307804267f808d82fde23f82cd32
23WH1A0505 <150135423+23WH1A0505@users.noreply.github.com>
2024-10-18 11:28:57 +0530

    Add files via upload

---
+++ 23wh1a0505/Assignment14.java
@@ -0,0 +1,86 @@
+
+package project;
+
+
+
+import java.util.Scanner;
+
+
+
+class CreditsNotSufficientException extends Exception {
+
+    public CreditsNotSufficientException(String message) {
+
+        super(message);
+
+    }
+
+}
+
+
+
+class CreditChecker {
+
+    public void checkCredits(int credits) throws CreditsNotSufficientException {
+
+        if (credits < 50) {
+
+            throw new CreditsNotSufficientException("Insufficient credits: " + credits);
+
+        }
+
+        System.out.println("Sufficient credits: " + credits);
+
+    }
+
+}
+
+
+
+public class Assignment14 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        CreditChecker checker = new CreditChecker();
+
+
+
+        try {
+
+            checker.checkCredits(30);
+
+        } catch (CreditsNotSufficientException e) {
+
+            System.out.println("Caught in main: " + e.getMessage());
+
+        }
+
+
+
+        anotherMethod(checker, 70);
+
+        anotherMethod(checker, 20);
+
+        scanner.close();
+
+    }
+
+
+
+    public static void anotherMethod(CreditChecker checker, int credits) {
+
+        try {
+
+            checker.checkCredits(credits);
+
+        } catch (CreditsNotSufficientException e) {
+
+            System.out.println("Caught in anotherMethod: " + e.getMessage());
+
+        }
+
+    }
+
+}
