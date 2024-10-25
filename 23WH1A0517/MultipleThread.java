9202cb973aa0aa55eb3fab1e0aa2425311a2c553
Sarah Sameera <145580784+sarah-bvrith517@users.noreply.github.com>
2024-10-25 11:51:46 +0530

    Create MultipleThread.java

---
+++ 23WH1A0517/MultipleThread.java
@@ -0,0 +1,61 @@
+package cse517;
+//Multiple Thread
+import java.util.Scanner;
+import java.lang.Thread;
+import java.util.Random; 
+class GenerateRandom extends Thread{
+	public void run() {
+		Scanner sc = new Scanner(System.in);
+		Random num = new Random();
+		int choice;
+		do {
+			int randnum = num.nextInt(100);
+			System.out.println("Generate Random Number is "+ randnum);
+			if (randnum % 2 == 0) {
+				EvenThread e= new EvenThread(randnum);
+				e.start();
+			}
+			else {
+				OddThread o = new OddThread(randnum);
+				o.start();
+			}
+			try {
+				Thread.sleep(1000);
+			}
+			catch(InterruptedException e){
+				System.out.println(e);
+			}
+			System.out.println("Do you want to continue the process(press 1 or 0):");
+			choice = sc.nextInt();
+		}
+		while(choice == 1);
+		sc.close();
+		System.out.println("The process is terminated");
+	}
+}
+class EvenThread extends Thread{
+	int num;
+	EvenThread(int randnum){
+		num = randnum;
+	}
+	public void run() {
+		System.out.println("The Square of "+num+" is: "+num*num);
+	}
+}
+class OddThread extends Thread{
+	int num;
+	OddThread(int randnum){
+		num = randnum;
+	}
+	public void run() {
+		System.out.println("The Cube of "+num+" is: "+num*num*num);
+	}
+}
+public class MultipleThread {
+
+	public static void main(String[] args) {
+		GenerateRandom g = new GenerateRandom(); 
+		g.start();
+	}
+
+}
