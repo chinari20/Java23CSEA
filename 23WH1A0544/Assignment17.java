708182134cf22f3fe215f1d603bcf40a570538fd
23wh1a0544 <150228254+23wh1a0544@users.noreply.github.com>
2024-10-25 10:50:00 +0530

    Add files via upload

---
+++ 23WH1A0544/Assignment17.java
@@ -0,0 +1,182 @@
+package source;
+import java.util.Random;
+
+import java.util.Scanner;
+
+
+
+class RandomNumberGenerator extends Thread {
+
+    private final NumberProcessor processor;
+
+    private volatile boolean running = true; 
+
+
+
+    public RandomNumberGenerator(NumberProcessor processor) {
+
+        this.processor = processor;
+
+    }
+
+
+
+    @Override
+
+    public void run() {
+
+        Random random = new Random();
+
+        while (running) {
+
+            int number = random.nextInt(100); 
+
+            System.out.println("Generated: " + number);
+
+            processor.processNumber(number);
+
+            try {
+
+                Thread.sleep(1000); 
+
+            } catch (InterruptedException e) {
+
+                Thread.currentThread().interrupt();
+
+                break;
+
+            }
+
+        }
+
+    }
+
+
+
+    public void stopRunning() {
+
+        running = false; 
+
+    }
+
+}
+
+
+
+class NumberProcessor {
+
+    public synchronized void processNumber(int number) {
+
+        if (number % 2 == 0) {
+
+            new SquareCalculator(number).start();
+
+        } else {
+
+            new CubeCalculator(number).start();
+
+        }
+
+    }
+
+}
+
+
+
+class SquareCalculator extends Thread {
+
+    private final int number;
+
+
+
+    public SquareCalculator(int number) {
+
+        this.number = number;
+
+    }
+
+
+
+    @Override
+
+    public void run() {
+
+        int square = number * number;
+
+        System.out.println("Square of " + number + ": " + square);
+
+    }
+
+}
+
+
+
+class CubeCalculator extends Thread {
+
+    private final int number;
+
+
+
+    public CubeCalculator(int number) {
+
+        this.number = number;
+
+    }
+
+
+
+    @Override
+
+    public void run() {
+
+        int cube = number * number * number;
+
+        System.out.println("Cube of " + number + ": " + cube);
+
+    }
+
+}
+
+
+
+public class Assignment17 {
+
+    public static void main(String[] args) {
+
+        NumberProcessor processor = new NumberProcessor();
+
+        RandomNumberGenerator generator = new RandomNumberGenerator(processor);
+
+        generator.start();
+
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Press Enter to stop...");
+
+        scanner.nextLine(); 
+
+
+
+        generator.stopRunning(); 
+
+        try {
+
+            generator.join(); 
+
+        } catch (InterruptedException e) {
+
+            e.printStackTrace();
+
+        }
+
+
+
+        System.out.println("Stopped the generator.");
+
+        scanner.close();
+
+    }
+
+}
